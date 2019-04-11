a1ca79956aaf764d594b3526f44560673f30787b
ZONICA LOMBARD <26995662@student365.msfed.nwu.ac.za>
2019-04-11 10:16:59 +0200

    Add project files.

---
+++ Paging/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Paging
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(277, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(226, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Page:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(277, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Add page";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(451, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Amount of page slots (1 -10):";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(29, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(142, 22);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(689, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 29);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Start paging";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn1.Location = new System.Drawing.Point(5, 193);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(94, 78);
+            this.btn1.TabIndex = 10;
+            this.btn1.Text = "empty";
+            this.btn1.UseVisualStyleBackColor = false;
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn9.Location = new System.Drawing.Point(805, 193);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(94, 78);
+            this.btn9.TabIndex = 11;
+            this.btn9.Text = "empty";
+            this.btn9.UseVisualStyleBackColor = false;
+            // 
+            // btn10
+            // 
+            this.btn10.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn10.Location = new System.Drawing.Point(905, 193);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(94, 78);
+            this.btn10.TabIndex = 12;
+            this.btn10.Text = "empty";
+            this.btn10.UseVisualStyleBackColor = false;
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn5.Location = new System.Drawing.Point(405, 193);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(94, 78);
+            this.btn5.TabIndex = 13;
+            this.btn5.Text = "empty";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btn4.Location = new System.Drawing.Point(305, 193);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(94, 78);
+            this.btn4.TabIndex = 14;
+            this.btn4.Text = "empty";
+            this.btn4.UseVisualStyleBackColor = false;
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn3.Location = new System.Drawing.Point(205, 193);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(94, 78);
+            this.btn3.TabIndex = 15;
+            this.btn3.Text = "empty";
+            this.btn3.UseVisualStyleBackColor = false;
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btn2.Location = new System.Drawing.Point(105, 193);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(94, 78);
+            this.btn2.TabIndex = 16;
+            this.btn2.Text = "empty";
+            this.btn2.UseVisualStyleBackColor = false;
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn6.Location = new System.Drawing.Point(505, 193);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(94, 78);
+            this.btn6.TabIndex = 17;
+            this.btn6.Text = "empty";
+            this.btn6.UseVisualStyleBackColor = false;
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btn7.Location = new System.Drawing.Point(605, 193);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(94, 78);
+            this.btn7.TabIndex = 18;
+            this.btn7.Text = "empty";
+            this.btn7.UseVisualStyleBackColor = false;
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn8.Location = new System.Drawing.Point(705, 193);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(94, 78);
+            this.btn8.TabIndex = 19;
+            this.btn8.Text = "empty";
+            this.btn8.UseVisualStyleBackColor = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(29, 325);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(929, 250);
+            this.textBox3.TabIndex = 20;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(44, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 29);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Add slots";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 629);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Paging - FIFO Algorithm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Paging/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paging
+{
+    public partial class Form1 : Form
+    {
+        private int firstclick = 0;
+        private int slots;
+        private int count;
+        private string reference;
+        private string[] positions;
+        private int counter;
+        private int index;
+        private int doubles;
+        private int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Properties.Settings.Default.Start = "0";
+            count = 0;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Set all button (slots) visibility to false
+            btn1.Visible = false;
+            btn2.Visible = false;
+            btn3.Visible = false;
+            btn4.Visible = false;
+            btn5.Visible = false;
+            btn6.Visible = false;
+            btn7.Visible = false;
+            btn8.Visible = false;
+            btn9.Visible = false;
+            btn10.Visible = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ELiminate , infront of string (reference list)
+            if (textBox1.Text != String.Empty)
+            {
+                if (firstclick == 0)
+                {
+                    label2.Text += textBox1.Text;
+                    firstclick++;
+                }
+                else
+                {
+                    label2.Text += "," + textBox1.Text;
+                }
+                count++;
+                textBox3.AppendText(textBox1.Text + " added to reference list. ");
+            }
+
+            reference = label2.Text;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) //start button
+        {
+            //After paging starts disable add buttons
+            button1.Visible = false;
+            button3.Visible = false;
+
+            textBox3.AppendText("Amount of slots available: " + textBox2.Text);
+            textBox3.AppendText(" Paging started ...");
+
+            positions = new string[count];
+            positions = reference.Split(',');
+
+            counter = 1; // to see that if counter is equal to the amount of slots paging should start
+            index = 1;   // btn shown
+
+           doubles = 0; //to check if slot already taken
+
+            //Start paging method
+            runpaging();
+        }
+        public void addPage(int index, int slotpos) {
+
+            switch (index) {
+                case 1: btn1.Text = positions[slotpos];
+                    break;
+                case 2:
+                    btn2.Text = positions[slotpos];
+                    break;
+                case 3:
+                    btn3.Text = positions[slotpos];
+                    break;
+                case 4:
+                    btn4.Text = positions[slotpos];
+                    break;
+                case 5:
+                    btn5.Text = positions[slotpos];
+                    break;
+                case 6:
+                    btn6.Text = positions[slotpos];
+                    break;
+                case 7:
+                    btn7.Text = positions[slotpos];
+                    break;
+                case 8:
+                    btn8.Text = positions[slotpos];
+                    break;
+                case 9:
+                    btn9.Text = positions[slotpos];
+                    break;
+                case 10:
+                    btn10.Text = positions[slotpos];
+                    break;
+
+
+            }
+        }
+        
+        public void runpaging() {
+
+
+            timer1.Start();
+            
+
+           }
+        
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            slots = Int32.Parse(textBox2.Text);
+
+
+            for (int i = 1; i <= slots; i++)
+            {
+                switch (i) {
+
+                    case 1: btn1.Visible = true;
+                        break;
+                    case 2:
+                        btn2.Visible = true;
+                        break;
+                    case 3:
+                        btn3.Visible = true;
+                        break;
+                    case 4:
+                        btn4.Visible = true;
+                        break;
+                    case 5:
+                        btn5.Visible = true;
+                        break;
+                    case 6:
+                        btn6.Visible = true;
+                        break;
+                    case 7:
+                        btn7.Visible = true;
+                        break;
+                    case 8:
+                        btn8.Visible = true;
+                        break;
+                    case 9:
+                        btn9.Visible = true;
+                        break;
+                    case 10:
+                        btn10.Visible = true;
+                        break;
+
+                }
+                
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // for (int i = 0; i < count; i++)
+            //  {
+            if (i < count)
+            {
+                if (i >= slots)
+                {
+                    for (int j = i - slots; j < i; j++) //check if page already exists
+                    {
+
+                        if (positions[j] == positions[i])
+                        {
+                            doubles++;
+                        }
+                    }
+                }
+                else {
+                    for (int j = 0; j < i; j++) //check if page already exists
+                    {
+
+                        if (positions[j] == positions[i])
+                        {
+                            doubles++;
+                        }
+                    }
+                }
+
+                if (index > slots)
+                {
+
+                    index = 1;
+
+                }
+                if (counter > slots)
+                {
+                    Console.WriteLine("Page fault occured. Remove slot " + positions[index] + " from memory");
+                    textBox3.AppendText("Page fault occured. Remove slot " + positions[index] + " from memory. ");
+                }
+
+                if (doubles == 0)
+                {
+                    addPage(index, i);
+                    index++;
+                    Console.Write(positions[i] + " add slot to memory");
+                    textBox3.AppendText(positions[i] + " add slot to memory. ");
+                    counter++;
+                }
+                else
+                {
+                    Console.Write(positions[i] + " slot already added to memory");
+                    textBox3.AppendText(positions[i] + " slot already added to memory. ");
+                }
+
+                doubles = 0;
+                i++;
+            }
+            if (i == positions.Length)
+            {
+                textBox3.AppendText("...Paging completed");
+                timer1.Stop();
+            }
+        }
+    }
+}
+
+
+//ESTIAN TLB en PAGE FRAMES
